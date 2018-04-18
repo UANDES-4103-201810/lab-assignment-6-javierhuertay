f114981bbbe78dbab06c568adc8f53b38769dc9b
Javier <jahuerta1@miuandes.cl>
2018-04-17 21:37:01 -0300

    -a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authorize
-  protected
-  def authorize
-	unless User.find_by(id: session[:user_id])
-		redirect_to login_url	
+
   def index
 
   end
